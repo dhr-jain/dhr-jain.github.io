7e232787b4f714244b47932ab4f6a268a30512e3
Dhruv Jain <123241810+dhr-jain@users.noreply.github.com>
2024-06-11 00:25:37 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,15 @@ window.addEventListener("load", function() {
     const loaderContainer = document.querySelector(".loader-container");
     loaderContainer.style.display = "none";
 });
+// GSAP animation for the name
+document.addEventListener('DOMContentLoaded', function() {
+    gsap.registerPlugin(TextPlugin);
+    gsap.to(".logo img", {duration: 2, text: "Dhruv Jain"});
+});
+
+// Toggle hamburger menu
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
+});
+
